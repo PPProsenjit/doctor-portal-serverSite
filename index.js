3315d17d714df00c3b51a5d9ee065772a2aacf16
PPProsenjite <prosenjitpappu03455@gmial.com>
2022-11-23 11:57:20 +0600

    bookedoption

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const port = process.env.Port || 5000;
 
 
@@ -8,6 +11,46 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xg2xq3m.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+
+        const appointmentOptionCollection = client.db('doctorPortal').collection('appointmentOptions');
+        const bookingsCollection = client.db('doctorPortal').collection('bookings');
+        
+        app.get('/appointmentOptions', async(req, res) =>{
+            const date= req.query.date;
+            const query = {};
+            const options = await appointmentOptionCollection.find(query).toArray();
+            const bookingQuery = {appointmentDate: date }
+            const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+            
+            options.forEach(option =>{
+                const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
+                const bookedSlots = optionBooked.map(book => book.slot);
+                console.log(date, option.name,bookedSlots);
+            })
+            res.send(options);
+        });
+
+        app.post('/bookings', async(req, res) => {
+            const booking = req.body;
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
+
+        })
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.log())
 
 app.get('/',async (req,res) => {
     res.send('Doctors portal server is running');
